292af5e2aafce423277ab98b91e9263569e1a381
tomas64test <git-commit-address>
2022-06-20 22:26:23 -0300

    adding files

--- Inc-product_file_reader.py
+++ Inc-product_file_reader.py
@@ -1,9 +1,10 @@
 #! Python 3.0
 #! Inc-product_file_reader.py
-#! File that reads a .xls and sorts the info in differents lists.
+#! File that reads a .xls and sorts info in differents lists.
 
 import os, openpyxl
 import pyexcel as p
+import module_total
 
 pathfolder = (os.path.dirname(__file__))
 filefolder = (pathfolder + "\\archivos\\")
@@ -20,21 +21,35 @@ else:
     wb = openpyxl.load_workbook(fileinc)
     ws = wb.active
 
-# Sacando SUCURSAL
+def cant(prod_total_cell,cant_total_cell):
+    if prod_total_cell in produc:
+        print(int(cant_total_cell / produc.get(prod_total_cell)))
+
+# Diccionario para cantidades sumadas
+productos_cant = {}
+
+# Sacando datos para imprimir
 suc_value = 0
-for suc_row in ws.iter_rows(min_row=2, max_row=ws.max_row -1, max_col=1):
-    for cell in suc_row:
-        if cell.value != suc_value:
-            print(cell.value)
-            
-            
-            # Hacer funcion
-            # for oc_row in ws.iter_rows(min_row=2, max_row=ws.max_row -1, max_col=2):
-            #     for cell in oc_row:
-            #         print(cell.value)
-            #         continue
+for cell in range (2, ws.max_row-1):
+    suc_cell = ws["A" + str(cell)].value                # Suc data
+    oc_cell = (ws["B" + str(cell)].value)               # OC data
+    prod_cell = (ws["H" + str(cell)].value)             # Producto data
+    cant_cell = (ws["I" + str(cell)].value)             # Cantidad data
+    if suc_cell == None:
+        continue
+    elif suc_cell != suc_value:
+        prt_cell = f'''Sucursal: {suc_cell}
+  OC: {str(oc_cell)}
+    Producto: {prod_cell} : {cant_cell}'''
+        print(prt_cell)
+        if module_total.cant(prod_cell,cant_cell) == True:
+            print(module_total.cant(prod_cell,cant_cell))
+    else:
+        prt_cell = f'''    Producto: {prod_cell} : {cant_cell}'''
+        print(prt_cell)     
+        # if module_total.cant(prod_cell,cant_cell) == True:
+        #     module_total.cant(prod_cell,cant_cell)
+    suc_value = suc_cell
 
-        suc_value = cell.value
-
-
+print(productos_cant)
 wb.close()
